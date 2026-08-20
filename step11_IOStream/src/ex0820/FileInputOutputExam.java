da9c4e13c606c00b21b0b5ea497cff82e469c309
vincecater1030 <vincecater1030@gmail.com>
2026-08-20 17:30:05 +0900

    0820 class

--- step11_IOStream/src/ex0820/FileInputOutputExam.java
+++ step11_IOStream/src/ex0820/FileInputOutputExam.java
@@ -1,12 +1,13 @@
 package ex0820;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class FileInputOutputExam {
 	public FileInputOutputExam() {
 		FileInputStream fis = null;
-
+		FileOutputStream fos = null;
 		try {
 			fis = new FileInputStream("src/ex0820/a.txt");
 			/*
@@ -30,8 +31,22 @@ public class FileInputOutputExam {
 			// byte [] 를 String 변환 방법
 			String str = new String(b);
 			System.out.println(str);
-
 			System.out.println("**완료**");
+
+			// 파일에 내용저장=쓰기
+			fos = new FileOutputStream("src/ex0820/save.txt");
+			// fos = new FileOutputStream("src/ex0820/save.txt", true);
+
+			/*
+			 * fos.write(65); fos.write(66); fos.write(67);
+			 * 
+			 * fos.write(13); fos.write(10);
+			 * 
+			 * fos.write(70); // String -> byte[]배열로 변환하면 좋은데..그래서 String data =
+			 * "집에 가고 싶어요~"; fos.write(data.getBytes());
+			 */
+
+			fos.write(b);
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -39,6 +54,8 @@ public class FileInputOutputExam {
 			try {
 				if (fis != null)
 					fis.close();
+				if (fos != null)
+					fos.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 
